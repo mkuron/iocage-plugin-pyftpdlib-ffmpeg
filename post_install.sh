3baeb4a1c9896303cb31b6957b0dfb3c5fac4a49
Michael Kuron <m.kuron@gmx.de>
2020-01-18 11:13:19 +0100

    initscript and not pip

--- post_install.sh
+++ post_install.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
 
-portsnap fetch
-portsnap extract
-export ALLOW_UNSUPPORTED_SYSTEM=1
+curl -L https://files.pythonhosted.org/packages/27/64/06a574350c79873e908fa9f48b617e7961de50fc468acc0a05d76771bce9/pyftpdlib-1.5.5.tar.gz | tar xzf -
+cd pyftpdlib-1.5.5
+python3 setup.py install
+cd ..
 
-mkdir -p /var/db/ports/ftp_py-pyftpdlib
-echo "OPTIONS_FILE_UNSET+=EXAMPLES" >> /var/db/ports/ftp_py-pyftpdlib/options
-echo "OPTIONS_FILE_UNSET+=OPENSSL" >> /var/db/ports/ftp_py-pyftpdlib/options
-echo "OPTIONS_FILE_UNSET+=SENDFILE" >> /var/db/ports/ftp_py-pyftpdlib/options
+curl -L https://files.pythonhosted.org/packages/5a/8d/27969852f4e664525c3d070e44b2b719bc195f4d18c311c52e57bb93614e/iso-639-0.4.5.tar.gz | tar xzf -
+cd iso-639-0.4.5
+python3 setup.py install
+cd ..
 
-echo "BUILD_ALL_PYTHON_FLAVORS=yes" >> /etc/make.conf
-
-cd /usr/ports/ftp/py-pyftpdlib
-make install FLAVOR=py36
-make clean
-
-mkdir -p /var/db/ports/devel_py-pip
-echo "OPTIONS_FILE_UNSET+=DOCS" >> /var/db/ports/devel_py-pip/options
-
-cd /usr/ports/devel/py-pip
-make install FLAVOR=py36
-make clean
-
-python3 -m pip install iso-639
+cp ftpserver.rc /etc/rc.d/ftpserver
+chmod +x /etc/rc.d/ftpserver
 
 sysrc -f /etc/rc.conf cron_enable="NO"
+sysrc -f /etc/rc.conf ftpserver_enable="YES"
